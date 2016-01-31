e7f789eff51bb8c2bc1d931b028a599b7ebc0510
Sam Lewis <samuelewis@gmail.com>
2016-01-31 18:02:22 -0500

    Added ITFFF maker trigger and more configuration options

--- softnr.ino
+++ softnr.ino
@@ -10,8 +10,20 @@
 #include <DNSServer.h>
 #include <WiFiManager.h>
 
-const char* host = "184.106.153.149";
+const char* thingspeakHost = "184.106.153.149";
+const char* iftttHost = "maker.ifttt.com";
+const int httpPort = 80;
+
 String thingspeakKey;
+String iftttKey;
+String iftttTrigger;
+
+int height = 80;
+int triggerLevel = 40;
+
+int belowTriggerCount = 0;
+int requiredReadingCount = 6;
+boolean triggerSent = false;
 
 void setup() {
 
@@ -19,22 +31,30 @@ void setup() {
     
     wifiManager.resetSettings();
 
-    WiFiManagerParameter custom_param("thingspeak", "Thingspeak Key", "", 40);
-    wifiManager.addParameter(&custom_param);
-    
-    wifiManager.autoConnect("AutoConnectAP");
+    wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
 
-    thingspeakKey = custom_param.getValue(); 
+    WiFiManagerParameter thingspeakParam("thingspeak", "Thingspeak key", "", 40);
+    wifiManager.addParameter(&thingspeakParam);
+    WiFiManagerParameter iftttParam("iftttKey", "IFTTT key", "", 80);
+    wifiManager.addParameter(&iftttParam);
+    WiFiManagerParameter iftttTriggerParam("iftttTigger", "IFTTT trigger", "", 40);
+    wifiManager.addParameter(&iftttTriggerParam);
+    WiFiManagerParameter heightParam("height", "Height of water softener (cm)", "", 40);
+    wifiManager.addParameter(&heightParam);
+    WiFiManagerParameter triggerParam("trigger", "Trigger level (cm)", "", 40);
+    wifiManager.addParameter(&triggerParam);
+    
+    wifiManager.autoConnect("softnr config");
+
+    thingspeakKey = thingspeakParam.getValue(); 
+    iftttKey = iftttParam.getValue();
+    iftttTrigger = iftttTriggerParam.getValue();
+    height = (String(heightParam.getValue())).toInt();
+    triggerLevel = (String(triggerParam.getValue())).toInt();
+    delay(5000);
 }
 
 void loop() {
-  delay(5000);
-
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    return;
-  }
   int reading = analogRead(A0);
   if (reading == 0) {
     reading = 1;
@@ -45,20 +65,54 @@ void loop() {
     distance = 80;
   }
 
-  // We now create a URI for the request
-  String url = "/update?key=" + thingspeakKey;
-  url += "&field1=";
-  url += distance;
-  
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-  delay(10);
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
+  int remaining = height - distance;
+
+  if (remaining < triggerLevel) {
+     ++belowTriggerCount;
+  } else {
+    belowTriggerCount = 0;
+    triggerSent = false;
   }
+
+  if ((belowTriggerCount >= requiredReadingCount)) {
+    if (iftttKey.length() > 0 && iftttTrigger.length() > 0) {
+      if (triggerSent == false) {
+        triggerSent = true;
+        WiFiClient iftttClient;
+        if (iftttClient.connect(iftttHost, httpPort)) {
+          if (thingspeakKey.length() > 0) {
+            // We now create a URI for the request
+            String url = "/trigger/" + iftttTrigger;
+            url += "/with/key/";
+            url += iftttKey;
+            
+            // This will send the request to the server
+            iftttClient.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                         "Host: " + iftttHost + "\r\n" + 
+                         "Connection: close\r\n\r\n");
+          }
+        }
+      }
+    }
+  }
+
+  // Log reading to Thingspeak
+  if (thingspeakKey.length() > 0) {
+    
+    WiFiClient client;
+    if (client.connect(thingspeakHost, httpPort)) {
+      // We now create a URI for the request
+      String url = "/update?key=" + thingspeakKey;
+      url += "&field1=";
+      url += remaining;
+      
+      // This will send the request to the server
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                   "Host: " + thingspeakHost + "\r\n" + 
+                   "Connection: close\r\n\r\n");
+    }
+  }
+  
+  delay(60 * 60 * 1000);
 }
 
